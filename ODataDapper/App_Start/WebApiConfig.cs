380ffd6f13ff116f0eec5b5d567f39dc80533247
katarinakelam <katarina.kelam@enterwell.net>
2019-05-01 20:17:34 +0200

    logging not working

--- ODataDapper/App_Start/WebApiConfig.cs
+++ ODataDapper/App_Start/WebApiConfig.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web.Http;
 using System.Web.Http.OData.Builder;
 using System.Web.Http.OData.Extensions;
+using ODataDapper.Helpers;
 using ODataDapper.Models;
 
 namespace ODataDapper
@@ -25,6 +26,7 @@ namespace ODataDapper
             var addStavkaToRacunAction = builder.Entity<Racun>().Action("AddStavka");
             addStavkaToRacunAction.Parameter<StavkaDTO>("Value");
 
+            config.MessageHandlers.Add(new LoggingRequestHandler());
             config.Routes.MapODataServiceRoute(
                 routeName: "ODataRoute",
                 routePrefix: null,
---
+++ ODataDapper/Helpers/LoggingRequestHandler.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using Serilog;
+
+namespace ODataDapper.Helpers
+{
+    /// <summary>
+    /// The logging request handler helper.
+    /// </summary>
+    /// <seealso cref="DelegatingHandler" />
+    public class LoggingRequestHandler : DelegatingHandler
+    {
+        /// <summary>
+        /// Sends an HTTP request to the inner handler to send to the server as an asynchronous operation.
+        /// </summary>
+        /// <param name="request">The HTTP request message to send to the server.</param>
+        /// <param name="cancellationToken">A cancellation token to cancel operation.</param>
+        /// <returns>
+        /// Returns <see cref="T:System.Threading.Tasks.Task`1" />. The task object representing the asynchronous operation.
+        /// </returns>
+        protected override async Task<HttpResponseMessage> SendAsync(
+            HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            // log request body if it has one
+            if (request.Content != null)
+            {
+                var requestBody = await request.Content.ReadAsStringAsync();
+                Log.Logger.ForContext<LoggingRequestHandler>().Warning(requestBody);
+            }
+
+            // let other handlers process the request
+            var result = await base.SendAsync(request, cancellationToken);
+
+            // if response body exists, once it is ready, log it
+            if (result.Content != null)
+            {
+                var responseBody = await result.Content.ReadAsStringAsync();
+                Log.Logger.ForContext<LoggingRequestHandler>().Warning(responseBody);
+            }
+
+            return result;
+        }
+    }
+}
